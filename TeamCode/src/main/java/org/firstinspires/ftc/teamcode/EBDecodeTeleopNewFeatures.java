446ad9dd0e042578b4cfb8fa705a0847cb1f1391
EB-Programmer <eb_programmer@yahoo.com>
2026-04-25 14:10:54 -0400

    update new features opmode to work with multiple color sensors and LED indicators

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EBDecodeTeleopNewFeatures.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EBDecodeTeleopNewFeatures.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.SwitchableLight;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
@@ -21,15 +22,14 @@ import java.util.Locale;
  * This OpMode tests new robot features and hardware
  *
  * Hardware Required:
- *   colorSensor1
- *   ledIndicator
+ *   colorSensor0, colorSensor1, colorSensor2
+ *   ledIndicator0, ledIndicator1
  *   webcam
  *
  * Controls for Gamepad 1:
- *   A:              Change LED indicator color
- *   X:              Toggle April Tag processor on/off
- *   DPad Up:        Adjust color sensor gain up
- *   DPad Down:      Adjust color sensor gain down
+ *   X:               Toggle April Tag processor on/off
+ *   DPad Up/Down:    Adjust color sensor gain up/down
+ *   DPad Right/Left: Adjust artifact distance threshold up/down
  *
  * Controls for Gamepad 2:
  *
@@ -37,19 +37,35 @@ import java.util.Locale;
 
 @TeleOp(group="EBDecodeTest")
 public class EBDecodeTeleopNewFeatures extends LinearOpMode {
+    private static final double COLOR_OFF = 0.0;
+    private static final double COLOR_RED = 0.277;
+    private static final double COLOR_ORANGE = 0.333;
+    private static final double COLOR_YELLOW = 0.388;
+    private static final double COLOR_SAGE = 0.444;
+    private static final double COLOR_GREEN = 0.5;
+    private static final double COLOR_AZURE = 0.555;
+    private static final double COLOR_BLUE = 0.611;
+    private static final double COLOR_INDIGO = 0.666;
+    private static final double COLOR_VIOLET = 0.722;
+    private static final double COLOR_WHITE = 1.0;
+    private static final double[] COLOR_STATE = {COLOR_GREEN, COLOR_YELLOW, COLOR_ORANGE, COLOR_RED};
+    private static /*final*/ double ARTIFACT_DIST_THRESHOLD_CM = 2.0;
+    private static /*final*/ float COLOR_SENSOR_GAIN = 2.0f;
+    private static final int ARTIFACT_PRESENT_DEBOUNCE_MS = 500;
+
     private AprilTagProcessor aprilTagProcessor = null;
     private VisionPortal visionPortal = null;
-    private NormalizedColorSensor colorSensor1 = null;
-    private Servo ledIndicator = null;
+    private NormalizedColorSensor[] colorSensors = {null, null, null};
+    private Servo[] ledIndicators = {null, null};
 
     private static final int LOOP_PERIOD = 20;  // milliseconds
-    private static final double[] INDICATOR_COLOR_VALUES = {0.0, 0.277, 0.333, 0.388, 0.444, 0.5, 0.555, 0.611, 0.666, 0.722, 1.0};
 
     private AprilTagDetection aprilTagDetection = null;
-    private int indicatorColorIdx = 0;
-    private float colorSensorGain = 2.0f;
-    private NormalizedRGBA colorData1 = null;
-    private double colorDist1 = -1.0;
+    private NormalizedRGBA[] colorData = {null, null, null};
+    private double[] colorDists = {-1.0, -1.0, -1.0};
+    private boolean[] artifactPresent = {false, false, false};
+    private boolean[] prevArtifactPresent = {false, false, false};
+    private ElapsedTime[] artifactPresentTimer = {null, null, null};
 
 
     @Override
@@ -63,9 +79,11 @@ public class EBDecodeTeleopNewFeatures extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
+            updateConstants();
             updateAprilTag();
-            updateIndicator();
-            updateColorSensor();
+            updateColorSensors();
+            updateArtifactState();
+            updateIndicators();
             updateTelemetry();
 
             sleep(LOOP_PERIOD);
@@ -75,12 +93,16 @@ public class EBDecodeTeleopNewFeatures extends LinearOpMode {
     }
 
     public void initHardware() {
-        colorSensor1 = hardwareMap.get(NormalizedColorSensor.class, "colorSensor1");
-        if (colorSensor1 instanceof SwitchableLight) {
-            ((SwitchableLight)colorSensor1).enableLight(true);
+        for (int i = 0; i < colorSensors.length; i++) {
+            colorSensors[i] = hardwareMap.get(NormalizedColorSensor.class, "colorSensor" + i);
+            if (colorSensors[i] instanceof SwitchableLight) {
+                ((SwitchableLight) colorSensors[i]).enableLight(true);
+            }
         }
 
-        ledIndicator = hardwareMap.get(Servo.class, "ledIndicator");
+        for (int i = 0; i < ledIndicators.length; i++) {
+            ledIndicators[i] = hardwareMap.get(Servo.class, "ledIndicator" + i);
+        }
 
         // Initialize webcam and April Tag processor
         aprilTagProcessor = new AprilTagProcessor.Builder().build();
@@ -98,27 +120,62 @@ public class EBDecodeTeleopNewFeatures extends LinearOpMode {
         visionPortal.close();
     }
 
-    public void updateIndicator() {
-        if (gamepad1.aWasPressed()) {
-            indicatorColorIdx += 1;
-            indicatorColorIdx = indicatorColorIdx % INDICATOR_COLOR_VALUES.length;
-            ledIndicator.setPosition(INDICATOR_COLOR_VALUES[indicatorColorIdx]);
+    public void updateConstants() {
+        if (gamepad1.dpadUpWasPressed()) {
+            // Only increase the gain by a small amount, since this loop will occur multiple times per second.
+            COLOR_SENSOR_GAIN += 0.01f;
+        } else if (gamepad1.dpadDownWasPressed() && COLOR_SENSOR_GAIN > 1) {
+            // A gain of less than 1 will make the values smaller, which is not helpful.
+            COLOR_SENSOR_GAIN -= 0.01f;
+        }
+
+        if (gamepad1.dpadRightWasPressed()) {
+            ARTIFACT_DIST_THRESHOLD_CM += 0.1;
+        } else if (gamepad1.dpadLeftWasPressed() && ARTIFACT_DIST_THRESHOLD_CM > 0.15) {
+            ARTIFACT_DIST_THRESHOLD_CM -= 0.1;
         }
     }
 
-    public void updateColorSensor() {
-        if (gamepad1.dpadUpWasPressed()) {
-            // Only increase the gain by a small amount, since this loop will occur multiple times per second.
-            colorSensorGain += 0.01f;
-        } else if (gamepad1.dpadDownWasPressed() && colorSensorGain > 1) { // A gain of less than 1 will make the values smaller, which is not helpful.
-            colorSensorGain -= 0.01f;
+    public void updateIndicators() {
+        int artifactCount = 0;
+        for (int i = 0; i < artifactPresent.length; i++) {
+            if (artifactPresent[i]) {
+                artifactCount += 1;
+            }
         }
 
-        colorSensor1.setGain(colorSensorGain);
-        colorData1 = colorSensor1.getNormalizedColors();
+        for (int i = 0; i < ledIndicators.length; i++) {
+            if ((artifactCount == 3) && (System.currentTimeMillis() % 200 < 100)) {
+                // Blink when all 3 artifacts are present
+                ledIndicators[i].setPosition(COLOR_OFF);
+            } else {
+                ledIndicators[i].setPosition(COLOR_STATE[artifactCount]);
+            }
+        }
+    }
 
-        if (colorSensor1 instanceof DistanceSensor) {
-            colorDist1 = ((DistanceSensor) colorSensor1).getDistance(DistanceUnit.CM);
+    public void updateArtifactState() {
+        for (int i = 0; i < colorSensors.length; i++) {
+            boolean curArtifactPresent = (colorDists[i] < ARTIFACT_DIST_THRESHOLD_CM);
+            if (curArtifactPresent != prevArtifactPresent[i]) {
+                prevArtifactPresent[i] = curArtifactPresent;
+                artifactPresentTimer[i].reset();
+            }
+            if (prevArtifactPresent[i] != artifactPresent[i]
+                    && artifactPresentTimer[i].milliseconds() > ARTIFACT_PRESENT_DEBOUNCE_MS) {
+                artifactPresent[i] = prevArtifactPresent[i];
+            }
+        }
+    }
+
+    public void updateColorSensors() {
+        for (int i = 0; i < colorSensors.length; i++) {
+            colorSensors[i].setGain(COLOR_SENSOR_GAIN);
+            colorData[i] = colorSensors[i].getNormalizedColors();
+
+            if (colorSensors[i] instanceof DistanceSensor) {
+                colorDists[i] = ((DistanceSensor) colorSensors[i]).getDistance(DistanceUnit.CM);
+            }
         }
     }
 
@@ -140,10 +197,9 @@ public class EBDecodeTeleopNewFeatures extends LinearOpMode {
 
     public void updateTelemetry() {
         telemetry.addLine("Gamepad1 Controls:");
-        telemetry.addLine("  A:        Change LED indicator color");
-        telemetry.addLine("  X:        Toggle April Tag processor on/off");
-        telemetry.addLine("  Dpad Up:  Increase Color Sensor Gain");
-        telemetry.addLine("  Dpad Up:  Decrease Color Sensor Gain");
+        telemetry.addLine("  X:               Toggle April Tag processor on/off");
+        telemetry.addLine("  Dpad Up/Down:    Adjust Color Sensor Gain Up/Down");
+        telemetry.addLine("  Dpad Right/Left: Adjust Artifact Dist Threshold Up/Down");
         telemetry.addLine();
 
         String aprilTagDesc = "None";
@@ -162,14 +218,17 @@ public class EBDecodeTeleopNewFeatures extends LinearOpMode {
 
         telemetry.addData("April Tag Processor Running", visionPortal.getProcessorEnabled(aprilTagProcessor));
         telemetry.addData("April Tag", aprilTagDesc);
-        telemetry.addData("Color Sensor Gain", "%.3f", colorSensorGain);
-        telemetry.addLine()
-                .addData("Red", "%.3f", colorData1.red)
-                .addData("Green", "%.3f", colorData1.green)
-                .addData("Blue", "%.3f", colorData1.blue);
-        telemetry.addData("Proximity (cm)", "%.3f", colorDist1);
-        telemetry.addData("LED Indicator Value", "%.3f", INDICATOR_COLOR_VALUES[indicatorColorIdx]);
-
+        telemetry.addData("Color Sensor Gain", "%.2f", COLOR_SENSOR_GAIN);
+        telemetry.addData("Artifact Dist Threshold (cm)", "%.1f", ARTIFACT_DIST_THRESHOLD_CM);
+        for (int i = 0; i < colorSensors.length; i++) {
+            telemetry.addLine()
+                    .addData("R", "%.1f", colorData[i].red)
+                    .addData("G", "%.1f", colorData[i].green)
+                    .addData("B", "%.1f", colorData[i].blue)
+                    .addData("Dist", "%.1f", colorDists[i])
+                    .addData("Present(Instant)", prevArtifactPresent[i])
+                    .addData("Present(State)", artifactPresent[i]);
+        }
         telemetry.update();
     }
 }
